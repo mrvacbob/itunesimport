5bbfe31b8a05f82e78195b9082d6e7fb073e1485
Alexander Strange <astrange@ithinksw.com>
2008-01-26 00:03:48 +0000

    initial import

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iTunesImport
+//
+//  Created by Alexander Strange on 1/25/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
